45b98d07d4d255a420a3ab26771013d15309c127
ccellis <coleman.ellis@students.olin.edu>
2018-10-25 18:28:48 -0400

    Added CPU.v with opcode and funct defines

---
+++ CPU.v
@@ -0,0 +1,14 @@
+`define LW_OP    6'b100011
+`define SW_OP    6'b101011
+`define J_OP     6'b000010
+`define JAL_OP   6'b000011
+`define BEQ_OP   6'b000100
+`define BNE_OP   6'b000101
+`define XORI_OP  6'b001110
+`define ADDI_OP  6'b001000
+`define RTYPE_OP 6'b000000
+
+`define JR_FUNCT  6'b001000
+`define ADD_FUNCT 6'b100000
+`define SUB_FUNCT 6'b100010
+`define SLT_FUNCT 6'b101010
